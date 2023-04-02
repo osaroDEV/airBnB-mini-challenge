38d575e07899c26eeed3ffae75c2ff5d33b0195b
osarodev <osernon@gmail.com>
2023-04-02 20:13:56 +0100

    update data

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,7 +3,7 @@ export default [
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-        price: 136,
+        price: 247,
         coverImg: "./images/swimmer.png",
         stats: {
             rating: '5.0',
